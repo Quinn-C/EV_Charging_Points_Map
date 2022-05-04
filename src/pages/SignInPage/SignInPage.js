aa8cbf2087fd1d5acf66e58b476a3d5c88ce902c
Quinn-C <joanchenuk@gmail.com>
2022-05-04 12:34:47 +0100

    animation background added

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import SignInButton from '../../components/SignInButton/SignInButton.js';
 import './SignInPage.css'
+import ParticlesBg from 'particles-bg'
 
 
 export function SignInPage() {
+
     return (
-    <div className="SignInPage">
-      <h3 className="welcome-title"> Welcome to </h3>
-      <h1 className="name-app"> EV Charging Points Map </h1>
-        <SignInButton/>
-    </div>
+      <>
+        <div className="SignInPage">
+          <h3 className="welcome-title"> Welcome to </h3>
+          <h1 className="name-app"> EV Charging Points Map </h1>
+          <SignInButton/>
+            
+        </div>
+        <ParticlesBg type="circle" bg={true} class='ParticleBG'/>
+      </>
+
     )
 }
